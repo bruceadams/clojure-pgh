a38a1118a18f337bc87a044418f772dabcd65d66
Joe Gallo <joegallo@gmail.com>
2010-04-08 11:04:08 -0400

    Updates and fixes based on feedback from the meeting.
    Using reduce more frequently, instead of apply.  Using equality
    comparison of sequences, rather than strings. Other various and sundry
    changes.

--- 2010-04-07-enlive/euler/euler.clj
+++ 2010-04-07-enlive/euler/euler.clj
@@ -2,37 +2,31 @@
   ([x] true)
   ([x y] (zero? (mod x y)))
   ([x y & z] (and (evenly? x y)
-		  (apply evenly? (cons x z)))))
+                  (apply evenly? (cons x z)))))
 
-;; (defn evenly? [x & y]
-;;   (if (seq y)
-;;     (and (zero? (mod x (first y)))
-;; 	 (apply evenly? (cons x (rest y))))
-;;     true))
-
-(println 
+(println
  "Euler #1 :"
  (reduce + (filter #(or (evenly? % 3) (evenly? % 5))
-		   (take 999 (iterate inc 1)))))
+                   (range 1 1000))))
 
 (defn fib-seq []
-  ((fn rfib [a b] 
+  ((fn rfib [a b]
      (cons a (lazy-seq (rfib b (+ a b)))))
    0 1))
 
-(println 
+(println
  "Euler #2 :"
  (reduce + (filter even? (take-while #(< % 4000000) (fib-seq)))))
 
 (defn candidates [x]
   (filter #(evenly? x %)
-	  (take-while #(<= % (int (Math/sqrt x))) 
-		      (iterate inc 2))))
+          (take-while #(<= % (int (Math/sqrt x)))
+                      (iterate inc 2))))
 
 (defn factors [x]
   (let [c (candidates x)]
     (sort (into c
-		(map #(/ x %) c)))))
+                (map #(/ x %) c)))))
 
 (defn prime? [x]
   (empty? (factors x)))
@@ -45,38 +39,37 @@
     (if (empty? f)
       [x]
       (cons (first f)
-	    (lazy-seq (factorization (/ x (first f))))))))
+            (lazy-seq (factorization (/ x (first f))))))))
 
-(println 
+(println
  "Euler #3 :"
- (first (sort (comparator >) (factorization 600851475143))))
+ (last (factorization 600851475143)))
 
 (defn palindrome? [s]
   (let [n (str s)]
-    (= n (apply str (reverse n)))))
+    (= (seq n) (reverse n))))
 
-(def threes (range 100 999))
+(def threes (range 100 1000))
 
-(println 
+(println
  "Euler #4 :"
  (reduce max (filter palindrome?
-		     (apply concat
-			    (map #(map * (repeat %1) %2)
-				 threes
-				 (repeat threes))))))
+                     (apply concat
+                            (map #(map * (repeat %1) %2)
+                                 threes
+                                 (repeat threes))))))
 
 (defn gcd [x y]
   (let [a (min x y)
-	b (max x y)]
-    (if (zero? a) 
+        b (max x y)]
+    (if (zero? a)
       b
       (recur a (- b a)))))
 
 (defn lcm
-  ([x] x)
   ([x y] (/ (* x y) (gcd x y)))
-  ([x y & z] (apply lcm (cons (lcm x y) z))))
+  ([x y & more] (reduce lcm (lcm x y) more)))
 
 (println
  "Euler #5 :"
- (apply lcm (range 2 20)))
+ (apply lcm (range 2 21)))
